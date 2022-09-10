31b46e316768887334890094a67d51c795efe05f
Diogo Martins de Assis <74463348+diogomsz@users.noreply.github.com>
2022-09-10 20:38:43 -0300

    Update relatorio_vendas.py

--- relatorio_vendas.py
+++ relatorio_vendas.py
@@ -1,55 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Relatório de Vendas usando pandas e envio automático de e-mail com Python
-# >Meu github: https://github.com/diogomsz
-# >
-# >ATENÇÃO: Aplicação feita 100% na IDE JupyterLab
-
-# In[50]:
-
-
 import pandas as pd
 import win32com.client as win32
 
 # importar a base de dados
 tabela_vendas  = pd.read_excel("Vendas.xlsx")
 
-
-# In[51]:
-
-
 # visualizar a base de dados
-display(tabela_vendas)
-
-
-# In[52]:
-
+print(tabela_vendas)
 
 # faturamento por loja
 faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
-display(faturamento)
-
-
-# In[53]:
-
+print(faturamento)
 
 # quantidade de produtos vendidos por loja
 quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
-display(quantidade)
-
-
-# In[54]:
-
+print(quantidade)
 
 # ticket medio por produto  em cada loja
 ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
 ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
-display(ticket_medio)
-
-
-# In[55]:
-
+print(ticket_medio)
 
 # enviar um email com o relatorio
 outlook = win32.Dispatch('outlook.application')
@@ -77,4 +46,3 @@ mail.HTMLBody = f'''
 '''
 
 mail.Send()
-
